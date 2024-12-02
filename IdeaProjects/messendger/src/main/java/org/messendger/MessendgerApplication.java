3000f30aa585cacfe22a93ed8a30c800bd477dbc
Dangerinchik <danikrainchik2007@gmail.com>
2024-12-02 21:31:38 +0300

    first commit

---
+++ IdeaProjects/messendger/src/main/java/org/messendger/MessendgerApplication.java
@@ -0,0 +1,13 @@
+package org.messendger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessendgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessendgerApplication.class, args);
+    }
+
+}
---
+++ IdeaProjects/messendger/src/test/java/org/messendger/MessendgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.messendger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessendgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
